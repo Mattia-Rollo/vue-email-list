762754aaca7b0677aa4a7bff9f851357dad28ca3
Mattia-Rollo <ste.tia@hotmail.it>
2022-11-08 21:10:10 +0100

    fix promise() emails appears in the same time

--- script.js
+++ script.js
@@ -9,10 +9,12 @@
     },
     methods: {
         callApi(){
+          const array1 = []
           for(let i = 0; i < 11; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
             console.log(res.data.response);
-            this.listaEmail.push(res.data.response);
+            array1.push(res.data.response);
+            if(i === 10) return this.listaEmail = [...array1];
             });
           }
         }
